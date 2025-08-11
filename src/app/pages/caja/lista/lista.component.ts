2f768ec589fbcb6b31c13d0f90bb8991deb8a65a
Alexandra Oreza <alexandra.oreza@gmail.com>
2025-08-11 17:55:50 -0600

    feat(Caja/pago): Added pago form & dialog

--- src/app/pages/caja/lista/lista.component.ts
+++ src/app/pages/caja/lista/lista.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
@@ -10,10 +10,14 @@ import { Subject, forkJoin, switchMap, takeUntil, map } from 'rxjs';
 import { Subscription } from 'rxjs';
 import { DialogModule } from 'primeng/dialog';
 import { ServicioGeneralService } from '../../../layout/service/servicio-general/servicio-general.service';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-lista',
-  imports: [CommonModule, TableModule, ButtonModule, CardModule, FormsModule, DialogModule],
+  imports: [CommonModule, TableModule, ButtonModule, CardModule, FormsModule, DialogModule, InputGroupModule, InputGroupAddonModule, ReactiveFormsModule, InputTextModule, DropdownModule,],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.scss'
 })
@@ -30,37 +34,63 @@ export class ListaComponent implements OnInit {
   model: any = 'clientes';
   private subs: Subscription[] = [];
   displayObservationDialog = false;
+  displayPagoDialog = false;
   newObservation = {
     text: '',
     date: new Date()
   };
+
+  pagoForm!: FormGroup;
+  formasPago = [{ label: 'Efectivo', value: 'EFECTIVO' },
+  { label: 'Depósito Bancario', value: 'DEPOSITO' },
+  { label: 'Nota de Crédito', value: 'NOTA' }
+  ]
+  tiposPago = [{ label: 'Letra Vencida', value: 'VENCIDA' },
+  { label: 'Letra Anticipada', value: 'ANTICIPADA' },
+  { label: 'Letra Adelantada', value: 'ADELANTADA' }
+  ]
+
   loading = true;
-  constructor(private route: ActivatedRoute,
-    private routerBack: Router, private servicioGeneral: ServicioGeneralService, private realtimeService: DatarealtimeService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private routerBack: Router,
+    private servicioGeneral: ServicioGeneralService,
+    private fb: FormBuilder,) { }
 
   ngOnInit(): void {
-
+    this.initializeForm();
     this.loadData();
-
   }
 
+  initializeForm() {
+    this.pagoForm = this.fb.group({
+      importe: new FormControl('', Validators.required),
+      recibido: new FormControl('', Validators.required),
+      formaPago: new FormControl(null, Validators.required),
+      tipoPago: new FormControl(null, Validators.required),
+      cuentaDestino: new FormControl('', Validators.required),
+      cambio: new FormControl(''),
+    })
+  }
   /** carga los datos de todas las tablas con base en credito
    */
   loadData(): void {
-    const creditoId = this.route.snapshot.params['id'];
+    this.creditoId = this.route.snapshot.params['id'];
 
     //Obtenemos el credito y los datos de las tablas relacionadas al credito
-    this.servicioGeneral.get(`credito/${creditoId}`, {}, true).subscribe(credito => {
+    this.servicioGeneral.get(`credito/${this.creditoId}`, {}, true).subscribe(credito => {
       forkJoin({
         payments_paid: this.servicioGeneral.get(`pago_realizado`, {}, true),
         payments_pending: this.servicioGeneral.get(`pago_pendiente`, {}, true),
         customer: this.servicioGeneral.get(`customers/${credito.data.customer_id}`),
         vehicles: this.servicioGeneral.get(`vehicles/${credito.data.vehicle_id}`),
         sucursales: this.servicioGeneral.get('sucursal'),
-      }).subscribe(({ payments_paid, payments_pending, customer, vehicles, sucursales }) => {
+        observaciones: this.servicioGeneral.get('observacion', {}, true),
+      }).subscribe(({ payments_paid, payments_pending, customer, vehicles, sucursales, observaciones }) => {
         const payment_paidList = payments_paid.data;
         const payment_pendingList = payments_pending.data;
         const sucursalesList = sucursales.data;
+        const observacionList = observaciones.data;
 
         this.cliente = customer.data;
         this.vehicle = vehicles.data
@@ -76,7 +106,7 @@ export class ListaComponent implements OnInit {
             }
           });
 
-        const payment_pending = payment_pendingList.filter((c: any) => c.credito_id === Number(creditoId));
+        const payment_pending = payment_pendingList.filter((c: any) => c.credito_id === Number(this.creditoId));
 
         console.log("return de load after filter: credito:", credito)
         console.log("pagado: ", payment_paid);
@@ -85,7 +115,7 @@ export class ListaComponent implements OnInit {
 
         this.pagosPendientes = payment_pending || null;
         this.pagosRealizados = payment_paid || null;
-        this.observaciones = credito.data.observacion || null;
+        this.observaciones = credito.data.observacion || observacionList || null;
       })
     })
   }
@@ -119,14 +149,22 @@ export class ListaComponent implements OnInit {
    * @todo: esperar a que se vea bien el flujo de las observaciones para poder modificar esto
    */
   saveObservation() {
-    const clienteId = this.route.snapshot.params['id'];
+    const clienteID = this.cliente.id;
     const data = {
-      idcliente: clienteId,
+      customer_id: clienteID,
       observacion: this.newObservation.text,
-      fecha: this.newObservation.date.toISOString()
     };
 
-    this.realtimeService.createRecord('observaciones', data).subscribe({
+    this.servicioGeneral.post(`observacion`, data).subscribe({
+      next: (res) => {
+        console.log('Observación creada: ', res)
+      },
+      error: (err) => {
+        console.error('Error al crear la obsrvacion: ', err)
+      }
+    })
+
+    /* this.realtimeService.createRecord('observaciones', data).subscribe({
       next: (res) => {
         console.log('Observación creada:', res);
         this.closeDialog();
@@ -134,7 +172,7 @@ export class ListaComponent implements OnInit {
       error: (err) => {
         console.error('Error al crear observación:', err);
       }
-    });
+    }); */
   }
 }
 
